9a46bdcb2cfa6e25047e992393ee9607dd82bcdc
Guillermo Aguilar <gaguilar128@gmail.com>
2021-05-26 01:47:32 -0500

    updated readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,12 +20,4 @@ Header.propTypes = {
   title: PropTypes.string.isRequired,
 }
 
-// CSS in JS
-// const headingStyle = {
-//   color: 'red',
-//   backgroundColor: 'black',
-// }
-
-
-
 export default Header
